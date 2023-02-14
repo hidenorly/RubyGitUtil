1369c1ce7d0684802e5610dce9f6d4a99ddd5c22
hidenorly <twitte.harold@gmail.com>
2023-02-15 00:06:24 +0900

    Add test case for containCommitOnBranch?

--- TestGitUtil.rb
+++ TestGitUtil.rb
@@ -4,9 +4,11 @@ require_relative "GitUtil"
 require_relative "ExecUtil"
 
 class TestGitUtil < Minitest::Test
+	DEF_INITIAL_COMMIT = "614d5cdbf61c94ebc5691b7ba8499a394e2e6484" # This is for https://github.com/hidenorly/RubyGitUtil
+
 	def setup
 		exit() if !File.exist?(".git")
-		exit() if GitUtil.getActualTailCommitId(".") != "614d5cdbf61c94ebc5691b7ba8499a394e2e6484"
+		exit() if GitUtil.getActualTailCommitId(".") != DEF_INITIAL_COMMIT
 	end
 
 	def teardown
@@ -17,10 +19,14 @@ class TestGitUtil < Minitest::Test
 	end
 
 	def test_getActualTailCommitId
-		assert_equal "614d5cdbf61c94ebc5691b7ba8499a394e2e6484", GitUtil.getActualTailCommitId(".")
+		assert_equal DEF_INITIAL_COMMIT, GitUtil.getActualTailCommitId(".")
 	end
 
-	def test_test_getHeadCommitId
+	def test_getHeadCommitId
 		assert_equal true, GitUtil.isCommitId?( GitUtil.getHeadCommitId(".") )
+	end
+
+	def test_containCommitOnBranch
+		assert_equal true, GitUtil.containCommitOnBranch?(".", DEF_INITIAL_COMMIT)
 	end
 end
