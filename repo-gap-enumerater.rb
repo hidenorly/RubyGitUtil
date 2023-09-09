33e6c8777d2321db9d59324b60091a2c0f6c6d45
hidenorly <twitte.harold@gmail.com>
2023-09-10 04:25:17 +0900

    Add robust exclude

--- repo-gap-enumerater.rb
+++ repo-gap-enumerater.rb
@@ -18,39 +18,97 @@ require 'optparse'
 require_relative 'ExecUtil'
 require_relative 'TaskManager'
 require_relative "RepoUtil"
+require 'fileutils'
 
 class ExecCommit2Patch < TaskAsync
-	def initialize(srcDir, path, gitPath, gitOption, patchDir, options)
-		super("ExecCommit2Patch::#{srcDir}:#{path}:#{gitPath}")
+	def initialize(srcDir, gitPath, gitOption, patchDir, verbose)
+		super("ExecCommit2Patch::#{srcDir}:#{gitPath}")
 		@srcDir = srcDir
-		@path = path
 		@gitPath = gitPath
 		@gitOption = gitOption
 		@patchDir = patchDir
-		@options = options
+		@verbose = verbose
 	end
 
 	def execute
 		patchDir = "#{@patchDir}/#{RepoUtil.getFlatFilenameFromGitPath(@gitPath)}"
 		FileUtil.cleanupDirectory(patchDir, true, true)
 
-		gitPath = @srcDir+"/"+@path
+		gitPath = @srcDir+"/"+@gitPath
 		if( FileTest.directory?(gitPath) ) then
-			puts "\n#{gitPath}" if @options[:verbose]
+			puts "\n#{gitPath}" if @verbose
 			exec_cmd = ""
 			exec_cmd = "git format-patch --subject-prefix=\"\" --no-numbered  #{GitUtil.getTailCommitId(gitPath)}...HEAD #{@gitOption} -o #{Shellwords.shellescape(patchDir)}"
 			exec_cmd += " > #{patchDir}.log"
-			exec_cmd += (@options[:verbose] ? " 2>&1" : " 2>/dev/null")
+			exec_cmd += (@verbose ? " 2>&1" : " 2>/dev/null")
 
 			ExecUtil.execCmd(exec_cmd, gitPath, false)
 		else
-			puts "\nSkipping... #{gitPath} (not existed)" if @options[:verbose]
+			puts "\nSkipping... #{gitPath} (not existed)" if @verbose
 		end
 		_doneTask()
 	end
 end
 
 
+class ExecExcludePatch < TaskAsync
+	def initialize(srcDir, srcGitPath, dstDir, dstGitPath, patchDir, verbose, robustMode=True)
+		super("ExecExcludePatch::#{srcGitPath}")
+		@git = srcGitPath
+		@srcGitPath = srcDir+"/"+srcGitPath
+		@dstGitPath = dstDir+"/"+dstGitPath
+		@verbose = verbose
+		@patchDir = "#{patchDir}/#{RepoUtil.getFlatFilenameFromGitPath(srcGitPath)}"
+		if !FileTest.directory?(@patchDir) then
+			@patchDir = "#{patchDir}/#{RepoUtil.getFlatFilenameFromGitPath(dstGitPath)}"
+		end
+		@_commitIDs={}
+		@robustMode = robustMode
+	end
+
+	def _createCache(gitPath)
+		@_commitIDs = {}
+		commitIDs = GitUtil.getAllCommitIdList(gitPath)
+		commitIDs.each do | aCommitId |
+			@_commitIDs[ aCommitId.to_s ] = true
+		end
+		commitIDs.clear()
+	end
+
+	def _containCommitOnBranch?(commitId)
+		return @_commitIDs.has_key?(commitId)
+	end
+
+	def _isIncludedOnBranch?(gitPath, aPatchPath)
+		patchBody = FileUtil.readFileAsArray(aPatchPath)
+		targetCommitId = GitUtil.getCommitIdFromPatch(gitPath, patchBody, true, true, @robustMode)
+		return targetCommitId ? true : false
+	end
+
+	def execute
+		if FileTest.directory?(@patchDir) then
+			if( FileTest.directory?(@srcGitPath) ) then
+				puts "\n#{@git}:#{@patchDir}" if @verbose
+				_createCache(@srcGitPath)
+
+				patches = []
+				FileUtil.iteratePath(@patchDir, "\.patch$", patches, false, false)
+				patches.sort!
+				patches.each do |aPatchPath|
+					aCommit = GitUtil.parsePatch(aPatchPath)
+					if _containCommitOnBranch?(aCommit[:id]) || _isIncludedOnBranch?(@srcGitPath, aPatchPath) then
+						FileUtils.rm_f(aPatchPath)
+						puts "#{aPatchPath} is removed because it's included in specified upstream" if @verbose
+					end
+				end
+				FileUtil.removeDirectoryIfNoFile(@patchDir)
+			else
+				puts "\nSkipping... #{@git} (not existed)" if @verbose
+			end
+		end
+		_doneTask()
+	end
+end
 
 
 #---- main --------------------------
@@ -108,9 +166,6 @@ end.parse!
 options[:srcDir] = File.expand_path(options[:srcDir]) if options[:srcDir]
 options[:dstDir] = File.expand_path(options[:dstDir])
 
-# common
-taskMan = ThreadPool.new( options[:numOfThreads].to_i )
-
 if ( options[:srcDir] && !RepoUtil.isRepoDirectory?(options[:srcDir]) ) then
 	puts "-s #{options[:srcDir]} is not repo directory"
 	exit(-1)
@@ -128,8 +183,20 @@ if options[:srcDir] && options[:dstDir] then
 end
 
 # step - 1 : convert commit to .patch
-targetGits.each do | path, gitPath |
-	taskMan.addTask( ExecCommit2Patch.new(options[:srcDir], path, gitPath, options[:srcGitOpt], options[:output], options) )
+taskMan = ThreadPool.new( options[:numOfThreads].to_i )
+
+targetGits.each do | srcGitPath, dstGitPath |
+	taskMan.addTask( ExecCommit2Patch.new(options[:srcDir], srcGitPath, options[:srcGitOpt], options[:output], options[:verbose]) )
+end
+
+taskMan.executeAll()
+taskMan.finalize()
+
+# step - 2 : exclude existing commits in dstGitPath
+taskMan = ThreadPool.new( options[:numOfThreads].to_i )
+
+targetGits.each do | srcGitPath, dstGitPath |
+	taskMan.addTask( ExecExcludePatch.new(options[:dstDir], dstGitPath, options[:srcDir], srcGitPath, options[:output], options[:verbose], true) )
 end
 
 taskMan.executeAll()
